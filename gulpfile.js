e3561a25ca3b66681bb0877d9f601570e7e428d6
BuildTools <unconfigured@null.spigotmc.org>
2020-09-13 21:49:54 -0400

    install gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+﻿/// <binding AfterBuild='default' />
+var gulp = require('gulp');
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+
+function minify() {
+	return gulp.src("wwwroot/js/**/*.js")
+		.pipe(uglify())
+		.pipe(concat("dutchtreat.min.js"))
+		.pipe(gulp.dest("wwwroot/dist"));
+}
+
+exports.default = gulp.series(
+	minify
+);
